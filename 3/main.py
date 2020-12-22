3e5ee6206f32eca61bd0322255d10928295ad782
HacDan <david.mcclellan.p@gmail.com>
2020-12-22 12:27:59 -0500

    Day 3A

--- 3/main.py
+++ 3/main.py
@@ -5,23 +5,19 @@ lineNum = -1
 for l in f:
     lineMapSegment = []
     l = l.strip()
+    l = l * 250
     for c in l:
-        colNum = 0
         lineMapSegment.append(c.strip())
     mapSegment.append(lineMapSegment)
 tree = "#"
 blank = "."
 treeCount = 0
-
-x = 3
-y = 1
-while x <= 31:
+x = 0
+y = 0
+while y <= 323:
     x += 3
     y += 1
-    print(x)
-    print(y)
+    print(x, y)
     if mapSegment[y][x] == tree:
         treeCount += 1
-        print("Found a tree at: ", x, y)
-
-print(treeCount)
+        print(treeCount)
